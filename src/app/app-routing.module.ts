44fc256fd8a5381f143f62921bd3245f496606df
bezzinag <bezzinag@yahoo.com>
2025-06-13 22:45:43 +0200

    Add routing configuration and clean up app component template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ViewTestsComponent } from './components/view-tests/view-tests.component';
+import { TestDetailsComponent } from './components/test-details/test-details.component';
+import { AddTestComponent } from './components/add-test/add-test.component';
+import { UpdateTestComponent } from './components/update-test/update-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'tests', component: ViewTestsComponent },
+  { path: 'tests/:id', component: TestDetailsComponent },
+  { path: 'add-test', component: AddTestComponent },
+  { path: 'update-test/:id', component: UpdateTestComponent },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
