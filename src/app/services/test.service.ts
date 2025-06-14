1982e249ef4201198a81a11dc39acbdc760f39bd
bezzinag <bezzinag@yahoo.com>
2025-06-14 17:57:44 +0200

    Fix API URL in TestService to point to the correct endpoint for pet tests

--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -1,5 +1,3 @@
-// src/app/services/test.service.ts
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -9,7 +7,7 @@ import { Test } from '../models/test.model';
   providedIn: 'root'
 })
 export class TestService {
-  private readonly apiUrl = 'http://localhost:8080/api/tests';
+  private readonly apiUrl = 'http://localhost:8080/api/pettests';
 
   constructor(private http: HttpClient) {}
 
@@ -17,9 +15,20 @@ export class TestService {
     return this.http.get<Test[]>(this.apiUrl);
   }
 
-  // Optional methods you may implement later:
-  // getTestById(id: number): Observable<Test> { ... }
-  // addTest(test: Test): Observable<Test> { ... }
-  // updateTest(id: number, test: Test): Observable<Test> { ... }
-  // deleteTest(id: number): Observable<void> { ... }
+  // Future implementations:
+  // getTestById(id: number): Observable<Test> {
+  //   return this.http.get<Test>(`${this.apiUrl}/${id}`);
+  // }
+
+  // addTest(test: Test): Observable<Test> {
+  //   return this.http.post<Test>(this.apiUrl, test);
+  // }
+
+  // updateTest(id: number, test: Test): Observable<Test> {
+  //   return this.http.put<Test>(`${this.apiUrl}/${id}`, test);
+  // }
+
+  // deleteTest(id: number): Observable<void> {
+  //   return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  // }
 }
